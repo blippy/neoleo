cf393a91c7c0c24d33e536c124309641766d78eb
Mark Carter <alt.mcarter@gmail.com>
2017-03-25 13:13:11 +0000

    Added Forth spreadsheet saving words in common.fs

--- examples/common.fs
+++ examples/common.fs
@@ -2,3 +2,12 @@
 : down-cell "down-cell" 4xcmd ;
 : up-cell "up-cell" 4xcmd ;
 
+\ let's create words for saving the spreadsheet
+256 string sname \ spreadsheet name
+: sssn ( str -- ) sname strcpy ; \ set spreadsheet name
+"unnamed.oleo" sssn \ set a default spreadsheet name
+500 string cmd \ a buffer for holding commands
+: mk-sss-cmd  \ make spreadsheet command
+	"save-spreadsheet " cmd strcpy 
+	sname cmd  strcat ;
+: sss cmd 4xcmd ; \ save spreadsheet
