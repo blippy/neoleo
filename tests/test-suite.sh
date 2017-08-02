950bbbbe786c154f2df0bfa9f06ee5d0793076f6
Mark Carter <alt.mcarter@gmail.com>
2017-08-02 14:48:04 +0100

    Testing improvements
    Hopefully that should be "it"

--- tests/test-suite.sh
+++ tests/test-suite.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-neoleo ../examples/ref.oleo -H <<< "save-spreadsheet out/foo.oleo"
+OFILE=`mktemp`
+#echo $OFILE
+neoleo ../examples/ref.oleo -H <<< "save-spreadsheet $OFILE"
+mv $OFILE out/test-suite.oleo
+diff out/test-suite.oleo verified/test-suite.oleo
+exit $?
