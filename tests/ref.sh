8b581efef1864167406c4db12f51326ef02de8e8
Mark Carter <alt.mcarter@gmail.com>
2017-08-05 08:34:19 +0100

    renamed:    test-suite.sh -> ref.sh

--- tests/ref.sh
+++ tests/ref.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+# a simple test of the referencing of neoleo
+# Just verifies that the input matches the output
+# and that no regression has been introduced
 OFILE=`mktemp`
-#echo $OFILE
 neoleo ../examples/ref.oleo -H <<< "save-spreadsheet $OFILE"
 mv $OFILE out/test-suite.oleo
 diff out/test-suite.oleo verified/test-suite.oleo
