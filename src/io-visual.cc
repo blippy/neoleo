d298bb50569c23d96c7b8a8d1b7227e67a71d609
Mark Carter <alt.mcarter@gmail.com>
2017-09-01 23:12:34 +0100

    Updated visual mode

--- src/io-visual.cc
+++ src/io-visual.cc
@@ -8,6 +8,8 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <utility>
+#include <vector>
 
 
 #include "cell.h"
@@ -19,8 +21,11 @@
 
 using std::cout;
 using std::endl;
+using std::flush;
+using std::pair;
 using std::string;
 using std::to_string; 
+using std::vector;
 
 bool use_coloured_output = false;
 
@@ -72,19 +77,42 @@ ssize_t read_in(char* buf, int buf_size)
 
 enum meta_keys { K_NORM, // The default case. Just a normal char
 	K_UNK, // Unknown key sequence
-	K_DOWN, K_LEFT, K_RIGHT, K_UP };
+	K_UP, K_DOWN, K_LEFT, K_RIGHT, K_HOME, K_END};
 
 // read from stdin, interpreting escape sequences
 int read_and_cook(meta_keys *special)
 {
 	char buf[10] = {0}; // whole array is set to 0
-	ssize_t n = read_in(buf, sizeof(buf));
+	ssize_t n = read_in(buf, sizeof(buf)-1);
+	buf[sizeof(buf)-1] = 0; // as a backstop against run-away string, which shouldn't happen anyway
 	*special = K_NORM;
 	// when n ==1, it could be a normal char (inc. control)
 	// or just the normal ESC key.  However, if the buffer
 	// returns more than 1 char, then it is an escape
 	// seuqnece that needs to be interpreted
-	if(n>1) *special = K_UNK; // by default, we don't know what's going on
+	if(n>1) {
+		*special = K_UNK; // by default, we don't know what's going on
+
+		static vector<pair<string,meta_keys>> kmap = { 
+			{"\E[1~",	K_HOME},
+			{"\E[4~",	K_END},
+			{"\E[A", 	K_UP},
+			{"\E[B", 	K_DOWN},
+			{"\E[C", 	K_RIGHT},
+			{"\E[D", 	K_LEFT}
+		};
+
+		for(auto& km:kmap) {
+			if(km.first == buf) {
+				*special = km.second;
+				break;
+			}
+		}
+
+
+	}
+
+	/*
 	if(buf[0] == '\E' && buf[1] == '[') {
 		if(n == 3) {
 			switch(buf[2]) {
@@ -96,17 +124,32 @@ int read_and_cook(meta_keys *special)
 			}
 		}
 	}
+	*/
+
 	return buf[0];
 
 }
 
 
+static void
+gotorc(int r, int c)
+{
+	cout << "\E[" << r << ";" << c << "H"; // move to row/col
+}
+
+static void 
+cleareol()
+{
+	cout << "\E[K";
+}
+
 void
 show_cells(int rt, int ct)
 {
 	//cout << "102 OK Terminated by dot" << endl;
-	cout << "r" << curow << "c" << cucol << 
-		"\E[K" << "\n"; // and clear to end of line
+	cout << "r" << curow << "c" << cucol;
+	cleareol();
+	cout << "\n"; 
 	for(int r=1; r<=rt; ++r) {
 		cout << on_red("R" + pad_right(std::to_string(r), 3))  << " ";
 		for(int c=1; c<= ct ; ++c) {
@@ -135,12 +178,42 @@ void show_cells()
 static void
 edit_cell_visually(int display_row)
 {
-	cout << "\E[" << display_row << ";1H"; // move to row/col
+	auto gotoc = [&display_row](int c) { gotorc(display_row, c); };
+	gotoc(1);
 	std::string formula =  get_cell_formula_at(curow, cucol);
 	cout << formula;
+	int col = formula.size() +1;
+	gotoc(col);
+	cout << flush;
 	if(false) log_debug("edit_cell_visually(): display_row = " + to_string(display_row) 
 			+ ", curow=" + to_string(curow) + ", cucol=" + to_string(cucol) + ", formula:" 
 			+ formula);
+
+	bool more = true;
+	while(more) {
+		meta_keys special;
+		int ch = read_and_cook(&special);
+		//cout << c << "\n";
+		switch(special) {
+			case K_NORM:
+				if(ch == 10) more = false; // return key
+				break; 
+			case K_HOME:
+				col = 1;
+				break;
+			case K_LEFT:
+				col = std::max(1, col-1);
+				break;
+			case K_RIGHT:
+				col = col+1;
+				// fallthrough
+			case K_END:
+				col = std::min((int)formula.size()+1, 79); // TODO repleace hard-code
+				break;
+		}
+		gotoc(col);
+		cout << flush;
+	}
 
 	cout << " edit_cell_visually() TODO";
 }
