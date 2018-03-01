4525f41611e810e894effb0f23ecbc3479ce1ef6
Mark Carter <alt.mcarter@gmail.com>
2018-03-01 14:41:35 +0000

    Moved some funcs from io-term.cc to main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,25 @@
 #include <exception>
+#include <getopt.h>
 #include <iostream>
 #include <string>
 #include <unistd.h>
 #include <vector>
 
 #include "assert.h"
-//#include "atlast.h"
-//#include "atoleo.h"
 #include "basic.h"
 #include "cmd.h"
+#include "config.h"
 #include "defuns.h"
 #include "init.h"
 #include "io-abstract.h"
+#include "io-headless.h"
 #include "io-term.h"
 #include "io-utils.h"
+#include "list.h"
+#include "logging.h"
 #include "mdi.h"
 #include "tests.h"
-//#include "graph.h"
-//#include "mysql.h"
-//#include "print.h"
-#include "logging.h"
 #include "utils.h"
-#include "io-headless.h"
 
 #include "oleox.hpp"
 
@@ -117,6 +115,166 @@ init_maps_and_macros()
                 exit (69);
 	}
 }
+
+
+static bool	option_tests = false;
+std::string	option_tests_argument = "regular";
+static char	option_separator = '\t';
+static char	*option_format = NULL;
+int		option_filter = 0;
+
+bool get_option_tests() { return option_tests;}
+
+static char short_options[] = "4:VqfxHhsFSTv";
+static struct option long_options[] =
+{
+	{"version",		0,	NULL,	'V'},
+	{"quiet",		0,	NULL,	'q'},
+	{"ignore-init-file",	0,	NULL,	'f'},
+	{"headless",		0,	NULL,	'H'},
+	{"help",		0,	NULL,	'h'},
+	{"separator",		1,	NULL,	's'},
+	{"space",		0,	NULL,	'S'},
+	{"format",		1,	NULL,	'F'},
+	{"filter",		0,	NULL,	'-'},
+	{"tests",		optional_argument,	NULL,	'T'},
+	{"version",		0,	NULL,	'v'},
+	{NULL,			0,	NULL,	0}
+};
+
+
+void
+print_version()
+{
+	printf(_("%s %s\n"), PACKAGE_NAME, VERSION);
+	printf(_("Copyright © 1992-2000 Free Software Foundation, Inc.\n"));
+	printf(_("%s comes with ABSOLUTELY NO WARRANTY.\n"), PACKAGE_NAME);
+	printf(_("You may redistribute copies of %s\n"), PACKAGE);
+	printf(_("under the terms of the GNU General Public License.\n"));
+	printf(_("For more information about these matters, "));
+	printf(_("see the files named COPYING.\n"));
+	printf("\nCompiled: %s %s\n", __DATE__, __TIME__);
+	printf("Datadir: %s/neoleo\n", DATADIR);
+	// will probably call exit after printing this
+}
+
+
+
+static void
+show_usage (void)
+{
+
+  printf(_("This is %s %s\n\n"), PACKAGE, VERSION);
+
+  printf(_("\
+Usage: %s [OPTION]... [FILE]...\n\
+"), PACKAGE);
+  printf(_("\
+\n\
+  -H, --headless           run without all toolkits\n\
+  -h, --help               display this help and exit\n\
+  -V, --version            output version information and exit\n\
+  -q, --quiet              do not display startup messages\n\
+  -f, --ignore-init-file   ignore settings defined in init file\n\
+  -s x, --separator x	   set separator for 'list' file type to x\n\
+  -S, --space		   set separator for 'list' file type to a space\n\
+  -T, --tests [x]          run test suite x\n\
+  -F x, --format x	   set default file type to x (oleo, list, sc  ...)\n\
+  --filter		   read file from stdin, write to stdout on exit\n\
+\n\
+Report bugs to <bug-oleo@gnu.org>.\n\
+"));
+}
+
+
+
+void
+parse_command_line(int argc, char **argv, volatile int *ignore_init_file)
+{
+	int opt, optindex;
+
+	while (1) {
+		opt = getopt_long (argc, argv, short_options, long_options, &optindex);
+		if (opt == EOF)
+			break;
+
+#if 0
+		if (opt)
+			fprintf(stderr, PACKAGE " option %c\n", opt);
+		else {
+			fprintf(stderr, PACKAGE " optind %d option %s strange ...\n",
+					optind, argv[optind]);
+		}
+#endif
+
+		switch (opt)
+		{
+			case 'v':
+			case 'V':
+				print_version();
+				exit (0);
+				break;
+			//case 'q':
+			//	spread_quietly = 1;
+			//	break;
+			case 'f':
+				*ignore_init_file = 1;
+				break;
+			case 'H':
+				user_wants_headless = true;
+				break;
+			case 'h':
+				show_usage ();
+				exit (0);
+				break;
+			case 's':
+				option_separator = argv[optind][0];
+#if 0
+				fprintf(stderr, PACKAGE " set list file separator to %c\n", option_separator);
+#endif
+				optind++;
+				list_set_separator(option_separator);
+				break;
+			case 'S':
+				option_separator = ' ';
+				list_set_separator(option_separator);
+				break;
+			case 'F':
+#if 0
+				fprintf(stderr, "F: optind %d argv[optind] '%s' optopt %d %c\n",
+						optind, argv[optind], optopt, optopt);
+#endif
+				option_format = argv[optind];
+				file_set_default_format(option_format);
+				optind++;
+				break;
+			case 'T':
+				option_tests = true;
+				//cout << "optindex:" << optind << "\n";
+				if(!optarg 
+						&& optind < argc
+						&& NULL !=argv[optind] 
+						&& '\0' != argv[optind][0]
+						&& '-' != argv[optind][0])
+					option_tests_argument = argv[optind++];
+				//exit(1);
+				break;
+			case '-':
+				option_filter = 1;
+				break;
+		}
+	}
+
+
+	if (argc - optind > 1)
+	{
+		show_usage ();
+		exit (1);
+	}
+}
+
+
+
 int 
 main(int argc, char **argv)
 {
