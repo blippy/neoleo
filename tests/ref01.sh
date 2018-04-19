465f9d440182c8c41075ef42308205a5f59245ea
Mark Carter <alt.mcarter@gmail.com>
2018-04-19 21:37:27 +0100

    Shuffled some tests around

--- tests/ref01.sh
+++ tests/ref01.sh
@@ -3,7 +3,7 @@
 # Just verifies that the input matches the output
 # and that no regression has been introduced
 TMPFILE=`mktemp`
-SS=$ASRCDIR/ref.oleo
+SS=$ASRCDIR/ref01.oleo
 echo "SS=$SS"
 neoleo $SS -H <<< "save-spreadsheet $TMPFILE"
 set_productions $SS
