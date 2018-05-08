ef7cecd053f75192f760b13e0e0196802f988f48
Mark Carter <alt.mcarter@gmail.com>
2018-05-08 11:28:26 +0100

    Prettify busi.cc

--- src/busi.cc
+++ src/busi.cc
@@ -36,17 +36,8 @@
 using std::cout;
 using std::endl;
 
-//#define Float	x.c_n
-//#define String	x.c_s
-//#define Int	x.c_l
-//#define Value	x.c_i
-//#define Rng	x.c_r
-
 static double
-pmt (
-     double principal,
-     double rate,
-     double term)
+pmt ( double principal, double rate, double term)
 {
   return (principal * rate) / (1 - pow (1 + rate, -(term)));
 }
@@ -107,20 +98,19 @@ know_f:
 static void
 do_pmt ( struct value *p)
 {
-  p->sFlt(pmt (p->gFlt(), (p + 1)->gFlt(), (p + 2)->gFlt()));
+	p->sFlt(pmt (p->gFlt(), (p + 1)->gFlt(), (p + 2)->gFlt()));
 }
 
 static void
-do_pv (
-     struct value *p)
+do_pv ( struct value *p)
 {
-  double payment, interest, term;
+	double payment, interest, term;
 
-  payment = p[0].gFlt();
-  interest = p[1].gFlt();
-  term = p[2].gFlt();
+	payment = p[0].gFlt();
+	interest = p[1].gFlt();
+	term = p[2].gFlt();
 
-  p->sFlt(payment * ((1 - pow (1 + interest, -term)) / interest));
+	p->sFlt(payment * ((1 - pow (1 + interest, -term)) / interest));
 }
 
 static void
@@ -143,444 +133,437 @@ do_npv ( struct value *p)
 static void
 do_irr (struct value *p)
 {
-  num_t try1;
-  num_t res;
-  num_t mint, maxt;
-  num_t  minr, maxr;
-  num_t i;
-  num_t tmp;
-  struct rng a_rng = p->gRng();
+	num_t try1;
+	num_t res;
+	num_t mint, maxt;
+	num_t  minr, maxr;
+	num_t i;
+	num_t tmp;
+	struct rng a_rng = p->gRng();
 
-  minr = maxr = 0;
-  mint = maxt = .1;  /* avoid divide by 0 in npv */
-  i = 0;
-  while (minr >= 0)
-    {
-      mint += 1;
-      tmp = npv (&a_rng, mint, &minr);
-      if (tmp)
+	minr = maxr = 0;
+	mint = maxt = .1;  /* avoid divide by 0 in npv */
+	i = 0;
+	while (minr >= 0)
 	{
-		p->sErr(tmp);
-	  //p->Value = tmp;
-	  //p->type = TYP_ERR;
-	  return;
+		mint += 1;
+		tmp = npv (&a_rng, mint, &minr);
+		if (tmp)
+		{
+			p->sErr(tmp);
+			//p->Value = tmp;
+			//p->type = TYP_ERR;
+			return;
+		}
+		if (++i == 40)
+		{
+			//p->Value = BAD_INPUT;
+			//p->type = TYP_ERR;
+			p->sErr(BAD_INPUT);
+			return;
+		}
 	}
-      if (++i == 40)
-        {
-	  //p->Value = BAD_INPUT;
-	  //p->type = TYP_ERR;
-	  p->sErr(BAD_INPUT);
-	  return;
-	}
-    }
-  i = 0;
-  while (maxr <= 0)
-    {
-      maxt -= 1;
-      tmp = npv (&a_rng, maxt, &maxr);
-      if (tmp)
+	i = 0;
+	while (maxr <= 0)
 	{
-	  //p->Value = tmp;
-	  //p->type = TYP_ERR;
-	  p->sErr(tmp);
-	  return;
+		maxt -= 1;
+		tmp = npv (&a_rng, maxt, &maxr);
+		if (tmp)
+		{
+			//p->Value = tmp;
+			//p->type = TYP_ERR;
+			p->sErr(tmp);
+			return;
+		}
+		if (++i == 40)
+		{
+			//p->Value = BAD_INPUT;
+			//p->type = TYP_ERR;
+			p->sErr(BAD_INPUT);
+			return;
+		}
 	}
-      if (++i == 40)
-        {
-	  //p->Value = BAD_INPUT;
-	  //p->type = TYP_ERR;
-	  p->sErr(BAD_INPUT);
-	  return;
-	}
-    }
-  try1 = (p + 1)->gFlt();
-  for (i = 0;; i++)
-    {
-      if (i == 40)
+	try1 = (p + 1)->gFlt();
+	for (i = 0;; i++)
 	{
-	  //p->Value = BAD_INPUT;
-	  //p->type = TYP_ERR;
-	  p->sErr(BAD_INPUT);
-	  return;
+		if (i == 40)
+		{
+			//p->Value = BAD_INPUT;
+			//p->type = TYP_ERR;
+			p->sErr(BAD_INPUT);
+			return;
+		}
+		tmp = npv (&a_rng, try1, &res);
+		if (tmp)
+		{
+			//p->Value = tmp;
+			//p->type = TYP_ERR;
+			p->sErr(tmp);
+			return;
+		}
+		if (fabs (res * 1000000.0) < 1)
+			break;
+		if (res > 0)
+		{
+			maxt = try1;
+			maxr = res;
+		}
+		else if (res < 0)
+		{
+			mint = try1;
+			minr = res;
+		}
+		if (minr / -10 > maxr)
+		{
+			/* it is quite near maxt */
+			try1 = (maxt * 10 + mint) / 11;
+		}
+		else if (minr / -2 > maxr)
+		{
+			try1 = (maxt * 2 + mint) / 3;
+		}
+		else if (minr * -10 < maxr)
+		{
+			/* It is quite near mint */
+			try1 = (maxt + mint * 10) / 11;
+		}
+		else if (minr * -2 < maxr)
+		{
+			try1 = (maxt + mint * 2) / 3;
+		}
+		else
+			try1 = (maxt + mint) / 2;
 	}
-      tmp = npv (&a_rng, try1, &res);
-      if (tmp)
-	{
-	  //p->Value = tmp;
-	  //p->type = TYP_ERR;
-	  p->sErr(tmp);
-	  return;
-	}
-      if (fabs (res * 1000000.0) < 1)
-	break;
-      if (res > 0)
-	{
-	  maxt = try1;
-	  maxr = res;
-	}
-      else if (res < 0)
-	{
-	  mint = try1;
-	  minr = res;
-	}
-      if (minr / -10 > maxr)
-	{
-	  /* it is quite near maxt */
-	  try1 = (maxt * 10 + mint) / 11;
-	}
-      else if (minr / -2 > maxr)
-	{
-	  try1 = (maxt * 2 + mint) / 3;
-	}
-      else if (minr * -10 < maxr)
-	{
-	  /* It is quite near mint */
-	  try1 = (maxt + mint * 10) / 11;
-	}
-      else if (minr * -2 < maxr)
-	{
-	  try1 = (maxt + mint * 2) / 3;
-	}
-      else
-	try1 = (maxt + mint) / 2;
-    }
-  p->sFlt(try1);
-  p->type = TYP_FLT;
+	p->sFlt(try1);
+	p->type = TYP_FLT;
 }
 
 
 static void
 do_fv ( struct value *p)
 {
-  double payment = p->gFlt();
-  double interest = (p + 1)->gFlt();
-  double term = (p + 2)->gFlt();
+	double payment = p->gFlt();
+	double interest = (p + 1)->gFlt();
+	double term = (p + 2)->gFlt();
 
-  p->sFlt(payment * (pow(1 + interest, term) - 1) / interest);
+	p->sFlt(payment * (pow(1 + interest, term) - 1) / interest);
 }
 
 static void
 do_rate ( struct value *p)
 {
-  double future = p->gFlt();
-  double present = (p + 1)->gFlt();
-  double term = (p + 2)->gFlt();
+	double future = p->gFlt();
+	double present = (p + 1)->gFlt();
+	double term = (p + 2)->gFlt();
 
-  p->sFlt(pow (future / present, 1 / term) - 1);
+	p->sFlt(pow (future / present, 1 / term) - 1);
 }
 
 static void
 do_term ( struct value *p)
 {
-  double payment = p->gFlt();
-  double interest = (p + 1)->gFlt();
-  double future = (p + 2)->gFlt();
+	double payment = p->gFlt();
+	double interest = (p + 1)->gFlt();
+	double future = (p + 2)->gFlt();
 
-  p->sFlt(log (1 + future * (interest / payment)) / log (1 + interest));
+	p->sFlt(log (1 + future * (interest / payment)) / log (1 + interest));
 }
 
 static void
-do_cterm (
-     struct value *p)
+do_cterm ( struct value *p)
 {
-  double interest = (p)->gFlt();
-  double future = (p + 1)->gFlt();
-  double present = (p + 2)->gFlt();
+	double interest = (p)->gFlt();
+	double future = (p + 1)->gFlt();
+	double present = (p + 2)->gFlt();
 
-  p->sFlt(log (future / present) / log (1 + interest));
+	p->sFlt(log (future / present) / log (1 + interest));
 }
 
 static void
-do_sln (
-     struct value *p)
+do_sln ( struct value *p)
 {
-  double cost = (p)->gFlt();
-  double salvage = (p + 1)->gFlt();
-  double life = (p + 2)->gFlt();
+	double cost = (p)->gFlt();
+	double salvage = (p + 1)->gFlt();
+	double life = (p + 2)->gFlt();
 
-  if (life < 1)
-    {
-      //p->Value = OUT_OF_RANGE;
-      //p->type = TYP_ERR;
-      p->sErr(OUT_OF_RANGE);
-      return;
-    }
-  p->sFlt((cost - salvage) / life);
+	if (life < 1)
+	{
+		//p->Value = OUT_OF_RANGE;
+		//p->type = TYP_ERR;
+		p->sErr(OUT_OF_RANGE);
+		return;
+	}
+	p->sFlt((cost - salvage) / life);
 }
 
 static void
 do_syd ( struct value *p)
 {
-  double cost, salvage, life, period;
+	double cost, salvage, life, period;
 
-  cost = p->gFlt();
-  salvage = (p + 1)->gFlt();
-  life = (p + 2)->gFlt();
-  period = (p + 3)->gFlt();
+	cost = p->gFlt();
+	salvage = (p + 1)->gFlt();
+	life = (p + 2)->gFlt();
+	period = (p + 3)->gFlt();
 
-  if (period < 1 || life < 1)
-    {
-      //p->Value = OUT_OF_RANGE;
-      //p->type = TYP_ERR;
-      p->sErr(OUT_OF_RANGE);
-      return;
-    }
-  if (period > life)
-    p->sFlt(0);
-  else if (period == life)
-    p->sFlt(salvage);
-  else
-    p->sFlt(((cost - salvage) * (life - period + 1)) / (life * ((life + 1) / 2)));
+	if (period < 1 || life < 1)
+	{
+		//p->Value = OUT_OF_RANGE;
+		//p->type = TYP_ERR;
+		p->sErr(OUT_OF_RANGE);
+		return;
+	}
+	if (period > life)
+		p->sFlt(0);
+	else if (period == life)
+		p->sFlt(salvage);
+	else
+		p->sFlt(((cost - salvage) * (life - period + 1)) / (life * ((life + 1) / 2)));
 }
 
 
 static void
-do_ddb (
-     struct value *p)
+do_ddb ( struct value *p)
 {
-  double cost = (p)->gFlt();
-  double salvage = (p + 1)->gFlt();
-  long life = (p + 2)->gInt();
-  long period = (p + 3)->gInt();
+	double cost = (p)->gFlt();
+	double salvage = (p + 1)->gFlt();
+	long life = (p + 2)->gInt();
+	long period = (p + 3)->gInt();
 
-  double bookval, tmp;
-  long n;
+	double bookval, tmp;
+	long n;
 
-  if (period < 1 || period > life || life < 1)
-    {
-      //p->Value = OUT_OF_RANGE;
-      //p->type = TYP_ERR;
-      p->sErr(OUT_OF_RANGE);
-      return;
-    }
-  bookval = cost;
-  tmp = 0;
-  for (n = 0; n < period; n++)
-    {
-      tmp = (bookval * 2) / life;
-      bookval -= tmp;
-      if (bookval < salvage)
+	if (period < 1 || period > life || life < 1)
 	{
-	  tmp += bookval - salvage;
-	  bookval = salvage;
+		//p->Value = OUT_OF_RANGE;
+		//p->type = TYP_ERR;
+		p->sErr(OUT_OF_RANGE);
+		return;
 	}
-    }
-  p->sFlt(tmp);
+	bookval = cost;
+	tmp = 0;
+	for (n = 0; n < period; n++)
+	{
+		tmp = (bookval * 2) / life;
+		bookval -= tmp;
+		if (bookval < salvage)
+		{
+			tmp += bookval - salvage;
+			bookval = salvage;
+		}
+	}
+	p->sFlt(tmp);
 }
 
 static void
-do_anrate (
-     struct value *p)
+do_anrate ( struct value *p)
 {
-  double in_pmt = (p)->gFlt();
-  double present = (p + 1)->gFlt();
-  double term = (p + 2)->gFlt();
+	double in_pmt = (p)->gFlt();
+	double present = (p + 1)->gFlt();
+	double term = (p + 2)->gFlt();
 
-  double tr_lo, tr_hi;
-  double mytry;
-  double try_pmt;
-  int n;
+	double tr_lo, tr_hi;
+	double mytry;
+	double try_pmt;
+	int n;
 
-  if (in_pmt * term == present)
-    {
-      p->sFlt(0.0);
-      return;
-    }
-  if (in_pmt * term < present)
-    {
-      tr_lo = -1;
-      tr_hi = 0;
-      while (pmt (present, tr_lo, term) > in_pmt)
+	if (in_pmt * term == present)
 	{
-	  tr_hi = tr_lo;
-	  tr_lo *= 2;
+		p->sFlt(0.0);
+		return;
 	}
-    }
-  else
-    {
-      tr_lo = 0;
-      tr_hi = 1;
-      while (pmt (present, tr_hi, term) < in_pmt)
+	if (in_pmt * term < present)
 	{
-	  tr_lo = tr_hi;
-	  tr_hi *= 2;
+		tr_lo = -1;
+		tr_hi = 0;
+		while (pmt (present, tr_lo, term) > in_pmt)
+		{
+			tr_hi = tr_lo;
+			tr_lo *= 2;
+		}
 	}
-    }
-  for (n = 0; n < 40; n++)
-    {
-      mytry = (tr_lo + tr_hi) / 2;
-      try_pmt = pmt (present, mytry, term);
-      if (try_pmt < in_pmt)
-	tr_lo = mytry;
-      else if (try_pmt > in_pmt)
-	tr_hi = mytry;
-      else
-	break;
-    }
-  p->sFlt(mytry);
+	else
+	{
+		tr_lo = 0;
+		tr_hi = 1;
+		while (pmt (present, tr_hi, term) < in_pmt)
+		{
+			tr_lo = tr_hi;
+			tr_hi *= 2;
+		}
+	}
+	for (n = 0; n < 40; n++)
+	{
+		mytry = (tr_lo + tr_hi) / 2;
+		try_pmt = pmt (present, mytry, term);
+		if (try_pmt < in_pmt)
+			tr_lo = mytry;
+		else if (try_pmt > in_pmt)
+			tr_hi = mytry;
+		else
+			break;
+	}
+	p->sFlt(mytry);
 }
 
 static void
 do_anterm ( struct value *p)
 {
-  double payment = (p)->gFlt();
-  double principal = (p + 1)->gFlt();
-  double rate = (p + 2)->gFlt();
+	double payment = (p)->gFlt();
+	double principal = (p + 1)->gFlt();
+	double rate = (p + 2)->gFlt();
 
-  p->sFlt((-log (1 - principal * (rate / payment))) / log (1 + rate));
+	p->sFlt((-log (1 - principal * (rate / payment))) / log (1 + rate));
 }
 
 
 static void
 do_balance ( struct value *p)
 {
-  double principal = (p)->gFlt();
-  double rate = (p + 1)->gFlt();
-  long term = (p + 2)->gInt();
-  long period = (p + 3)->gInt();
+	double principal = (p)->gFlt();
+	double rate = (p + 1)->gFlt();
+	long term = (p + 2)->gInt();
+	long period = (p + 3)->gInt();
 
-  double tmp_pmt, int_part;
-  long num;
+	double tmp_pmt, int_part;
+	long num;
 
-  if (term < period)
-    {
-      //p->Value = OUT_OF_RANGE;
-      //p->type = TYP_ERR;
-      p->sErr(OUT_OF_RANGE);
-      return;
-    }
-  tmp_pmt = pmt (principal, rate, (double) term);
-  for (num = 0; num < period; num++)
-    {
-      int_part = rate * principal;
-      if (int_part > tmp_pmt)
+	if (term < period)
 	{
-	  //p->Value = OUT_OF_RANGE;
-	  //p->type = TYP_ERR;
-	  p->sErr(OUT_OF_RANGE);
-	  return;
+		//p->Value = OUT_OF_RANGE;
+		//p->type = TYP_ERR;
+		p->sErr(OUT_OF_RANGE);
+		return;
 	}
-      principal -= tmp_pmt - int_part;
-    }
-  p->sFlt(principal);
+	tmp_pmt = pmt (principal, rate, (double) term);
+	for (num = 0; num < period; num++)
+	{
+		int_part = rate * principal;
+		if (int_part > tmp_pmt)
+		{
+			//p->Value = OUT_OF_RANGE;
+			//p->type = TYP_ERR;
+			p->sErr(OUT_OF_RANGE);
+			return;
+		}
+		principal -= tmp_pmt - int_part;
+	}
+	p->sFlt(principal);
 }
 
 static void
-do_paidint (
-     struct value *p)
+do_paidint ( struct value *p)
 {
-  double principal = (p)->gFlt();
-  double rate = (p + 1)->gFlt();
-  long term = (p + 2)->gInt();
-  long period = (p + 3)->gInt();
+	double principal = (p)->gFlt();
+	double rate = (p + 1)->gFlt();
+	long term = (p + 2)->gInt();
+	long period = (p + 3)->gInt();
 
-  double tmp_pmt, int_part, retval;
-  long num;
+	double tmp_pmt, int_part, retval;
+	long num;
 
-  if (term < period)
-    {
-      //p->Value = OUT_OF_RANGE;
-      //p->type = TYP_ERR;
-      p->sErr(OUT_OF_RANGE);
-      return;
-    }
-  tmp_pmt = pmt (principal, rate, (double) term);
-  retval = 0;
-  for (num = 0; num < period; num++)
-    {
-      int_part = rate * principal;
-      if (int_part > tmp_pmt)
+	if (term < period)
 	{
-	  //p->Value = OUT_OF_RANGE;
-	  //p->type = TYP_ERR;
-	  p->sErr(OUT_OF_RANGE);
-	  return;
+		//p->Value = OUT_OF_RANGE;
+		//p->type = TYP_ERR;
+		p->sErr(OUT_OF_RANGE);
+		return;
 	}
-      principal -= tmp_pmt - int_part;
-      retval += int_part;
-    }
-  p->sFlt(retval);
+	tmp_pmt = pmt (principal, rate, (double) term);
+	retval = 0;
+	for (num = 0; num < period; num++)
+	{
+		int_part = rate * principal;
+		if (int_part > tmp_pmt)
+		{
+			//p->Value = OUT_OF_RANGE;
+			//p->type = TYP_ERR;
+			p->sErr(OUT_OF_RANGE);
+			return;
+		}
+		principal -= tmp_pmt - int_part;
+		retval += int_part;
+	}
+	p->sFlt(retval);
 }
 
 static void
-do_kint (
-     struct value *p)
+do_kint ( struct value *p)
 {
-  double principal = (p)->gFlt();
-  double rate = (p + 1)->gFlt();
-  long term = (p + 2)->gInt();
-  long period = (p + 3)->gInt();
+	double principal = (p)->gFlt();
+	double rate = (p + 1)->gFlt();
+	long term = (p + 2)->gInt();
+	long period = (p + 3)->gInt();
 
-  double tmp_pmt, int_part = 0;
-  long num;
+	double tmp_pmt, int_part = 0;
+	long num;
 
-  if (term < period)
-    {
-      //p->Value = OUT_OF_RANGE;
-      //p->type = TYP_ERR;
-      p->sErr(OUT_OF_RANGE);
-      return;
-    }
-
-  tmp_pmt = pmt (principal, rate, (double) term);
-  for (num = 0; num < period; num++)
-    {
-      int_part = rate * principal;
-      if (int_part > tmp_pmt)
+	if (term < period)
 	{
-	  //p->Value = OUT_OF_RANGE;
-	  //p->type = TYP_ERR;
-	  p->sErr(OUT_OF_RANGE);
-	  return;
+		//p->Value = OUT_OF_RANGE;
+		//p->type = TYP_ERR;
+		p->sErr(OUT_OF_RANGE);
+		return;
 	}
-      principal -= tmp_pmt - int_part;
-    }
-  p->sFlt(int_part);
+
+	tmp_pmt = pmt (principal, rate, (double) term);
+	for (num = 0; num < period; num++)
+	{
+		int_part = rate * principal;
+		if (int_part > tmp_pmt)
+		{
+			//p->Value = OUT_OF_RANGE;
+			//p->type = TYP_ERR;
+			p->sErr(OUT_OF_RANGE);
+			return;
+		}
+		principal -= tmp_pmt - int_part;
+	}
+	p->sFlt(int_part);
 }
 
 static void
-do_kprin (
-     struct value *p)
+do_kprin ( struct value *p)
 {
-  double principal = (p)->gFlt();
-  double rate = (p + 1)->gFlt();
-  long term = (p + 2)->gInt();
-  long period = (p + 3)->gInt();
-  double tmp_pmt, int_part = 0;
-  long num;
+	double principal = (p)->gFlt();
+	double rate = (p + 1)->gFlt();
+	long term = (p + 2)->gInt();
+	long period = (p + 3)->gInt();
+	double tmp_pmt, int_part = 0;
+	long num;
 
-  if (term < period)
-    {
-      //p->Value = OUT_OF_RANGE;
-      //p->type = TYP_ERR;
-	  p->sErr(OUT_OF_RANGE);
-      return;
-    }
-
-  tmp_pmt = pmt (principal, rate, (double) term);
-  for (num = 0; num < period; num++)
-    {
-      int_part = rate * principal;
-      if (int_part > tmp_pmt)
+	if (term < period)
 	{
-	  //p->Value = OUT_OF_RANGE;
-	  //p->type = TYP_ERR;
-	  p->sErr(OUT_OF_RANGE);
-	  return;
+		//p->Value = OUT_OF_RANGE;
+		//p->type = TYP_ERR;
+		p->sErr(OUT_OF_RANGE);
+		return;
 	}
-      principal -= tmp_pmt - int_part;
-    }
-  p->sFlt(tmp_pmt - int_part);
+
+	tmp_pmt = pmt (principal, rate, (double) term);
+	for (num = 0; num < period; num++)
+	{
+		int_part = rate * principal;
+		if (int_part > tmp_pmt)
+		{
+			//p->Value = OUT_OF_RANGE;
+			//p->type = TYP_ERR;
+			p->sErr(OUT_OF_RANGE);
+			return;
+		}
+		principal -= tmp_pmt - int_part;
+	}
+	p->sFlt(tmp_pmt - int_part);
 }
 
 static void
 do_compbal (struct value *p)
 {
-  num_t principal = (p)->gFlt();
-  num_t rate = (p + 1)->gFlt();
-  num_t term = (p + 2)->gFlt();
+	num_t principal = (p)->gFlt();
+	num_t rate = (p + 1)->gFlt();
+	num_t term = (p + 2)->gFlt();
 
-  p->sFlt(principal * busi_pow (1 + rate, term));
+	p->sFlt(principal * busi_pow (1 + rate, term));
 }
 
 static void
