2ddd115a08a08b740caa060bd71b4503d8045136
Mark Carter <alt.mcarter@gmail.com>
2019-01-20 12:41:00 +0000

    Removed string function trim and mid
    Hopefully no-one was using them. If you were, then let me know,
    and I'll re-instate them.

--- src/stringo.cc
+++ src/stringo.cc
@@ -25,9 +25,6 @@
 #include <functional>
 #include <string.h>
 #include <ctype.h>
-#define obstack_chunk_alloc ck_malloc
-#define obstack_chunk_free free
-#include "obstack.h"
 
 #include "funcs.h"
 #include "global.h"
@@ -42,36 +39,12 @@
 
 #define Value	x.c_i
 
-#define ERROR(x)	\
- {			\
-	p->Value=x;	\
-	p->type=TYP_ERR;\
-	return;		\
- }
-	
-
-extern struct obstack tmp_mem;
-
-char *alloc_tmp_mem(size_t n)
-{
-	return (char *) obstack_alloc(&tmp_mem, n);
-}
-
-char * alloc_value_str(struct value* p)
-{
-	return alloc_tmp_mem(strlen(p->gString())+1);
-}
-
-
 std::function<void(struct value*)>
 wrapfunc(std::function<std::string(struct value*)> func)
 {
 	auto fn = [=](struct value* p) { 
 		try {
 			std::string s = func(p);
-			//char* ret = alloc_tmp_mem(s.size()+1);		
-			//strcpy(ret,  s.c_str());
-			//p->sString(ret);
 			p->sString(s.c_str());
 		} catch (int e) {
 			p->Value = e;
@@ -90,62 +63,6 @@ static void
 do_len(struct value * p)
 {
 	p->sInt(strlen(p->gString()));
-}
-
-
-
-
-
-
-static void
-do_trim_str(struct value * p)
-{
-	char *s1,*s2;
-	int sstart=0;
-	char *strptr;
-
-	strcpy_c s2c{p->gString()};
-	strptr=alloc_value_str(p);
-	for(s1=strptr,s2=s2c.data();*s2;s2++) {
-		if(!isascii(*s2) || !isprint(*s2))
-			continue;
-		if(*s2==' ') {
-				if(sstart) {
-				*s1++= *s2;
-				sstart=0;
-			}
-		} else {
-			sstart=1;
-			*s1++= *s2;
-		}
-	}
-	*s1=0;
-	p->sString(strptr);
-}
-
-
-static void
-do_mid(struct value * p)
-{
-	const char *str = p->gString();
-	long from = (p+1)->gInt()-1;
-	long len =  (p+2)->gInt();
-
-	char	*ptr1;
-	int tmp;
-
-	tmp=strlen(str);
-
-	if(from<0 || len<0)
-		ERROR(OUT_OF_RANGE);
-	ptr1=(char *)obstack_alloc(&tmp_mem,len+1);
-	if(from>=tmp || len==0)
-		ptr1[0]='\0';
-	else {
-		strncpy(ptr1,str+from,len);
-		ptr1[len]='\0';
-	}
-	p->sString(ptr1);
 }
 
 
@@ -188,8 +105,6 @@ static void do_edit_1(struct value*p) { wrapfunc(do_edit)(p);}
 
 function_t string_funs[] = {
 { C_FN1,	X_A1,	"S",    to_vptr(do_len),	"len" }, 
-{ C_FN1,	X_A1,	"S",    to_vptr(do_trim_str),	"trim" }, 
-{ C_FN3,	X_A3,	"SII",  to_vptr(do_mid),	"mid" }, 
 { C_FN3,	X_A3,	"SII", to_vptr(do_edit_1),	"edit" }, 
 { C_FN0,        X_A0,   "",    to_vptr(do_version_1),    "version" },
 { C_FN2,        X_A2,   "SS",    to_vptr(do_concata_1),    "concata" },
