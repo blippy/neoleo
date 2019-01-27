718503f4f5d086d70d76c363125ed21328588497
Mark Carter <alt.mcarter@gmail.com>
2019-01-27 14:57:19 +0000

    A semblance of a working ncurses alternative.
    Type C-q to quit. Keys up and down now work.
    
    Looking forward to the day that the terrible cmd.cc can be
    jettisoned.

--- src/io-2019.cc
+++ src/io-2019.cc
@@ -3,16 +3,41 @@
 #include <ncurses.h>
 
 #include "io-2019.h"
+#include "logging.h"
+#include "window.h"
 
 using std::cout;
 
+constexpr int CTRL(int c) { return c & 037; }
+
 void main_command_loop_for2019()
 {
+	// Tell ncusres to interpret "special keys". It means
+	// that KEY_DOWN etc. will work, but ESC won't be
+	// read separately
+	keypad(stdscr, TRUE);
+
 	while(1) {
-		char c = getch();
-		if(c == 'q') break;
+		int c = getch();
+		switch(c) {
+			case CTRL('q'):  
+				goto finis;
+			case KEY_DOWN:
+			case 'j':
+				log_debug("io-2019:down arrow");
+				io_shift_cell_cursor(1, 1);
+				break;
+			case KEY_UP:
+			case 'k':
+				io_shift_cell_cursor(0, 1);
+				break;
+
+		}
+
 	}
 
+finis:
+	endwin();
 	cout << "Exiting from 2019 io\n";
 	exit(0);
 
