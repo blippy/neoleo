0aba261b7ba4efe8998ae6ab9e635fc4ad3aa427
Mark Carter <alt.mcarter@gmail.com>
2019-01-30 18:56:20 +0000

    basic.cc doesn't need to include cmd.h

--- src/basic.cc
+++ src/basic.cc
@@ -1,7 +1,5 @@
 /*
- * $Id: basic.c,v 1.20 2000/07/22 06:13:15 danny Exp $
- *
- * Copyright © 1993, 2000 Free Software Foundation, Inc.
+ * Copyright (c) 1993, 2000 Free Software Foundation, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +26,6 @@
 
 #include "basic.h"
 #include "byte-compile.h"
-#include "cmd.h"
 #include "convert.h"
 #include "regions.h"
 #include "window.h"
