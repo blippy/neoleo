25e817f19fc375aef822de80d347fbd56464e503
Mark Carter <alt.mcarter@gmail.com>
2019-02-07 18:55:39 +0000

    Removed commented-out code in defuns.h

--- src/defuns.h
+++ src/defuns.h
@@ -1,8 +1,6 @@
 #pragma once
 /*
- * $Id: defuns.h,v 1.3 2000/07/22 06:13:15 danny Exp $
- *
- * Copyright © 1993, 2000 Free Software Foundation, Inc.
+ * Copyright (c) 1993, 2000 Free Software Foundation, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,110 +25,5 @@
  */
 
 
-/* First, build arrays containing FUNC_ARG strings. */
-
-/*
-#define DEFUN_5(Sname, FDname, FAname, DFname, Cname) \
-0, \
-}; \
-\
-static char * FAname [] = \
-{
-
-#define DEFUN(Sname, FDname, FAname, Cname) \
-	DEFUN_5(Sname, FDname, FAname, 0, Cname)
-
-#define DEFUN_3(Sname, FDname, Cname)
-
-#define FUNC_ARG_STRINGS	1
-
-static char * FAfnord[] = 
-{
-#include "defun.h"
-0,
-};
-
-// Docstrings 
-
-#undef DEFUN
-#undef DEFUN_3
-#undef DEFUN_5
-#undef FUNC_ARG_STRINGS
-#undef FUNC_INIT_CODE
-
-#define DEFUN_5(Sname, FDname, FAname, DFname, Cname) \
-0, \
-}; \
-\
-static char * FDname [] = \
-{
-
-#define DEFUN(Sname, FDname, FAname, Cname) \
-	DEFUN_5(Sname, FDname, FAname, 0, Cname)
-#define DEFUN_3(Sname, FDname, Cname) DEFUN(Sname, FDname, 0, Cname)
-
-
-#define DOC_STRINGS	1
-
-static char * FDfnord[] = 
-{
-  "Well, you know.",
-#include "defun.h"
-0,
-};
-
-
-// init_code 
-
-#undef DEFUN
-#undef DEFUN_3
-#undef DEFUN_5
-#undef FUNC_ARG_STRINGS
-#undef DOC_STRINGS
-#undef FUNC_INIT_CODE
-
-#define DEFUN_5(Sname, FDname, FAname, DFname, Cname) \
-0, \
-}; \
-\
-static char * DFname [] = \
-{
-
-#define DEFUN(Sname, FDname, FAname, Cname)
-#define DEFUN_3(Sname, FDname, Cname) 
-
-#define FUNC_INIT_CODE	1
-
-static char * DFfnord[] = 
-{
-#include "defun.h"
-0,
-};
-
-
-
-// Building the function table. 
-
-#undef DEFUN
-#undef DEFUN_3
-#undef DEFUN_5
-#undef FUNC_ARG_STRINGS
-#undef DOC_STRINGS
-#undef FUNC_INIT_CODE
-
-#define DEFUN_5(Sname, FDname, FAname, DFname, Cname) \
-	{ Sname, FDname, FAname, DFname, Cname },
-
-#define DEFUN(Sname, FDname, FAname, Cname) \
-	DEFUN_5(Sname, FDname, FAname, 0, Cname)
-#define DEFUN_3(Sname, FDname, Cname) \
-	DEFUN_5(Sname, FDname, 0, 0, Cname)
-*/
-
-
 struct cmd_func** get_cmd_funcs();
-
-
-
-
 void rewrite_defuns();
