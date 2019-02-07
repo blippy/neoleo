8d68adc392bb5263a6d42d8c64faa30b7c563422
Mark Carter <alt.mcarter@gmail.com>
2019-02-07 21:31:45 +0000

    Removed commented-out code

--- src/io-utils.cc
+++ src/io-utils.cc
@@ -484,12 +484,6 @@ std::string cell_value_string (CELLREF row, CELLREF col, int add_quote)
 
 		case TYP_STR:
 			return cp->get_formula_text();
-#if 0
-			{
-				strcpy_c s{cp->gString()};
-				return backslash_a_string (s.data(), add_quote);
-			}
-#endif
 
 		case TYP_BOL:
 			return bname[cp->gBol()];
