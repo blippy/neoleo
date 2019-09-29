9dd8126c86c0d1d61983f3c75c91edcceaeec527
Mark Carter <alt.mcarter@gmail.com>
2019-09-29 22:12:52 +0100

    parser.cc now does +,-,*,/

--- lab/parsing/parser.cc
+++ lab/parsing/parser.cc
@@ -16,72 +16,66 @@ using namespace std;
 
 class Expr;
 typedef vector<Expr> args_t;
-typedef function<int(args_t)> function_t;
+typedef double value_t;
+typedef function<value_t(args_t)> function_t;
 //class Args { public: vector<Expr> args; };
 class FunCall;
 class FunCall { public: function_t* fn ; args_t args; };
 class Expr { 
 	public: 
 		Expr() {};
-		Expr(int i) : expr(i) {};
-		/*
-		Expr(string fn, Expr arg1, Expr arg2)
-		{
-			FunCall fc;
-			fc.function_name = fn;
-			fc.args = vector<Expr>{arg1, arg2};
-			expr = fc;
-		};
-		*/
-		variant<FunCall, int> expr; 
+		Expr(value_t v) : expr(v) {};
+		variant<FunCall, value_t> expr; 
 };
-//typedef vector<Expr> args_t;
-//typedef function<int(args_t)> function_t;
-//class FunCall { public: string function_name ; function_t &fm ; vector<Expr> args; };
-//class FunCall { public: string function_name ; function_t &fm ; Args args; };
 
-/*
-class Expr { 
-	public: 
-		Expr() {};
-		Expr(int i) : expr(i) {};
-		Expr(string fn, Expr arg1, Expr arg2)
-		{
-			FunCall fc;
-			fc.function_name = fn;
-			fc.args = vector<Expr>{arg1, arg2};
-			expr = fc;
-		};
-		variant<FunCall, int> expr; 
-};
-*/
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // FUNCTION DEFINITIONS
 
-int eval(Expr expr);
+value_t eval(Expr expr);
 
 
 
-int do_plus(args_t args)
+value_t do_plus(args_t args)
 {
-	int val = 0;
+	value_t val = 0;
 	for(auto& arg: args) val += eval(arg);
 	return val;
 }
-int do_minus(args_t args)
+value_t do_minus(args_t args)
 {
 	if(args.size() == 0) return 0;
-	int val = eval(args[0]);
+	value_t val = eval(args[0]);
 	if(args.size() == 1) return -val; // if there is only one argument, then return the negative of it
 	for(int i = 1; i<args.size(); ++i) val -= eval(args[i]);
+	return val;
+}
+value_t do_mul(args_t args)
+{
+	value_t val = 1.0;
+	for(auto& arg: args) {
+	       	value_t a = eval(arg);
+		val *= a;
+		cout << "do_mul a and val " << a << " " << val << "\n";
+	}
+	return val;
+}
+value_t do_div(args_t args)
+{
+	if(args.size() == 0) return 0;
+	value_t val = eval(args[0]);
+	cout << "do_div 1/val " << 1.0/val << "\n";
+	if(args.size() == 1) return 1.0/val;
+	for(int i = 1; i<args.size(); ++i) val /= eval(args[i]);
 	return val;
 }
 
 map<string, function_t> funcmap= {
 	{"+", &do_plus},
-	{"-", &do_minus}
+	{"-", &do_minus},
+	{"*", &do_mul},
+	{"/", &do_div}
 };
 
 
@@ -135,16 +129,16 @@ void parse_error()
 }
 
 /*
-template<class R, class Q>
-R pop(Q& qs)
-{
-	R front = qs.front();
-	qs.pop_front();
-	return front;
-}
-*/
+   template<class R, class Q>
+   R pop(Q& qs)
+   {
+   R front = qs.front();
+   qs.pop_front();
+   return front;
+   }
+   */
 
-template<class Q>
+	template<class Q>
 Q rest(Q qs)
 {
 	qs.pop_front();
@@ -152,12 +146,12 @@ Q rest(Q qs)
 }
 
 /*
-tokens_t rest(tokens_t tokes)
-{
-	tokes.pop_front();
-	return tokes;
-}
-*/
+   tokens_t rest(tokens_t tokes)
+   {
+   tokes.pop_front();
+   return tokes;
+   }
+   */
 
 typedef deque<string> ops_t;
 
@@ -180,34 +174,31 @@ Expr parse_p(tokens_t& tokes)
 }
 Expr parse_f(tokens_t& tokes) { return parse_p(tokes); }
 
-Expr parse_t(tokens_t& tokes) { return parse_f(tokes); }
-
-
-Expr parse_e(tokens_t& tokes)
+Expr parse_tXXX(tokens_t& tokes) 
 {
 	FunCall fc; // assume it's a funcall for now
-	//fc.function_name = "+";
-	fc.fn = &funcmap["+"];
-	fc.args.push_back(parse_t(tokes));
-
-	for(;;) {
-		token_t toke = tokes.front();
-		tokes.pop_front();
-		if(toke.first == EOI) break;
-	       	if(toke.first == '+')
-			fc.args.push_back(parse_t(tokes));
-		else if(toke.first == '-') {
-			Expr eneg =parse_t(tokes);
-			FunCall fneg;
-			fneg.fn = &funcmap["-"];
-			fneg.args = args_t{eneg};
-			Expr x2;
-			x2.expr = fneg;
-			fc.args.push_back(x2);
-		} else
-		       	parse_error();
+	fc.fn = &funcmap["*"];
+	fc.args.push_back(parse_f(tokes));
+	/*
+	   for(;;) {
+	   cout << "looping parse_t\n";
+	   token_t toke = tokes.front();
+	   tokes.pop_front();
+	   if(toke.first == EOI) break;
+	   if(toke.first == '*')
+	   fc.args.push_back(parse_f(tokes));
+	   else if(toke.first == '/') {
+	   Expr eneg =parse_f(tokes);
+	   FunCall fneg;
+	   fneg.fn = &funcmap["/"];
+	   fneg.args = args_t{eneg};
+	   Expr x2;
+	   x2.expr = fneg;
+	   fc.args.push_back(x2);
+	   } else break; //else
+	//   	parse_error();
 	}
-
+	*/
 
 	if(fc.args.size() == 1)
 		return Expr(fc.args[0]);
@@ -218,6 +209,90 @@ Expr parse_e(tokens_t& tokes)
 	}
 }
 
+FunCall _parse_t(tokens_t& tokes)
+{
+	FunCall fc;
+	fc.fn = &funcmap["*"];
+
+	fc.args.push_back(parse_f(tokes));
+	//return fc;
+	while(1) {
+		auto nid = tokes.front().first;
+		if(nid == EOI) return fc;
+		if(nid == '*') {
+			tokes.pop_front();
+			fc.args.push_back(parse_f(tokes));
+		} else  if(nid == '/') {
+			tokes.pop_front();
+			Expr eneg =parse_f(tokes);
+			FunCall fneg;
+			fneg.fn = &funcmap["/"];
+			fneg.args = args_t{eneg};
+			Expr x2;
+			x2.expr = fneg;
+			fc.args.push_back(x2);		
+		} else {
+			return fc;
+		}
+	}
+}
+Expr parse_t(tokens_t& tokes)
+{
+	FunCall fc{_parse_t(tokes)};
+
+	if(fc.args.size() == 1)
+		return Expr(fc.args[0]);
+	else {
+		Expr x;
+		x.expr = fc;
+		return x;
+	}
+
+}
+
+FunCall _parse_e(tokens_t& tokes)
+{
+	FunCall fc;
+	fc.fn = &funcmap["+"];
+
+	fc.args.push_back(parse_t(tokes));
+	while(1) {
+		auto nid = tokes.front().first;
+		cout << "nid is " << nid << "\n";
+		if(nid == EOI) return fc;
+		//tokes.pop_front();
+		if(nid == '+') {
+			tokes.pop_front();
+			cout <<" nod is +\n";
+			fc.args.push_back(parse_t(tokes));
+		} else  if(nid == '-') {
+			tokes.pop_front();
+			Expr eneg =parse_t(tokes);
+			FunCall fneg;
+			fneg.fn = &funcmap["-"];
+			fneg.args = args_t{eneg};
+			Expr x2;
+			x2.expr = fneg;
+			fc.args.push_back(x2);		
+		} else {
+			return fc;
+		}
+	}
+}
+Expr parse_e(tokens_t& tokes)
+{
+	FunCall fc{_parse_e(tokes)};
+
+	if(fc.args.size() == 1)
+		return Expr(fc.args[0]);
+	else {
+		Expr x;
+		x.expr = fc;
+		return x;
+	}
+
+}
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -226,21 +301,16 @@ Expr parse_e(tokens_t& tokes)
 //template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 //template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-int eval(Expr expr)
+value_t eval(Expr expr)
 {
-	int val = 667;
-	if(std::holds_alternative<int>(expr.expr))
-		val = std::get<int>(expr.expr);
+	value_t val = 667;
+	if(std::holds_alternative<value_t>(expr.expr))
+		val = std::get<value_t>(expr.expr);
 	else { // must be a function call		
 		//auto &fn = std::get<FunCall>(expr.fn);
 		auto &fc = std::get<FunCall>(expr.expr);
 		auto fn = fc.fn;
 		return (*fn)(fc.args);
-		/*
-		if(fc.function_name == "+") {
-			return do_plus(fc.args);
-		}
-		*/
 	}
 
 	return val;
@@ -259,7 +329,7 @@ int interpret(string s, int expected)
 	}
 
 	Expr expr{parse_e(tokes)};
-	int val = eval(expr);
+	value_t val = eval(expr);
 	cout << "Evaluates to " << val << " ";
 	if(val == expected) 
 		cout << "PASS";
@@ -276,5 +346,8 @@ int main()
 	interpret("1+3+5+7", 16);
 	interpret("1-3-5-7", -14);
 	interpret("1-3+5-7", -4);
+	interpret("1+2*3", 7);
+	interpret("1+12/3", 5);
+	interpret("1+12/3*2/2", 5);
 	return 0;
 }
