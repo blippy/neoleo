1aae3923f6825f2f6b946285103a18094327c897
Mark Carter <alt.mcarter@gmail.com>
2020-01-05 15:10:00 +0000

    Removed uncalled functions in args.cc

--- src/args.cc
+++ src/args.cc
@@ -94,131 +94,47 @@ init_named_macro_strings (void)
 char_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	if (!**end)
-		return "No character specified";
-	else
-	{
-		int ch = string_to_char (end);
-		if (ch < 0)
-		{
-			setn_edit_line ("", 0);
-			return "Illegal character constant.";
-		}
-		else
-		{
-			arg->val.integer = ch;
-			return 0;
-		}
-	}
+	return 0;
 }
 
 	const char *
 symbol_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	char * e = *end;
-	char * start = *end;
-	if (isalpha (*e) || (*e == '-') || (*e == '_') || (*e == (Global->a0 ? '$' : ':')))
-	{
-		while (isalpha(*e) || isdigit(*e) || (*e == '-') || (*e == '_')
-				|| (*e == (Global->a0 ? '$' : ':')))
-			++e;
-		if (!isspace(*e) && *e)
-			goto bad_symbol;
-		*end = e;
-		arg->val.string = (char *)ck_malloc (e - start + 1);
-		if (e - start)
-			bcopy (start, arg->val.string, e - start);
-		arg->val.string[e - start] = '\0';
-		return 0;
-	}
-bad_symbol:
-	if (arg->arg_desc[1] == '\'')
-	{
-		arg->val.string = 0;
-		return 0;
-	}
-	else
-		return "Invalid symbol name.";
+	return 0;
 }
 
 	const char *
 word_verify ( char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	char * e = *end;
-	char * start = *end;
-	if (!isspace (*e))
-	{
-		while (*e && !isspace(*e))
-			++e;
-		*end = e;
-		arg->val.string = (char *)ck_malloc (e - start + 1);
-		if (e - start)
-			bcopy (start, arg->val.string, e - start);
-		arg->val.string[e - start] = '\0';
-		return 0;
-	}
-	else if (arg->arg_desc[1] == '\'')
-	{
-		arg->val.string = 0;
-		return 0;
-	}
-	else
-		return "Invalid symbol name.";
+	return 0;
 }
 
 	void
 symbol_destroy (struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	if (arg->val.string)
-		ck_free (arg->val.string);
 }
 
 	const char *
 command_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	const char * error = symbol_verify (end, arg);
-	char * str;
-	if (error)
-		return error;
-	str = arg->val.string;
-	if (!(find_function (0, 0, arg->val.string, strlen(arg->val.string))
-				&& get_abs_rng (&str, 0)))
-		return 0;
-	else
-		return "Not a command or macro address.";
+	return 0;
 }
 
 	const char * 
 read_file_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	FILE * fp = xopen_with_backup (arg->text.buf, "r");
-	*end = 0;
-	if (!fp)
-	{
-		io_error_msg ("Can't open file '%s':%s", arg->text.buf, err_msg ());
-		return "";
-	}
-	else
-	{
-		arg->val.fp = fp;
-		return 0;
-	}
+	return 0;
 }
 
 	void
 read_file_destroy (struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	int num;
-	num = xclose (arg->val.fp);
-	if (num)
-		io_error_msg ("Can't close '%s': Error code %d: %s",
-				arg->text.buf, num, err_msg ());
 }
 
 
@@ -226,30 +142,13 @@ read_file_destroy (struct command_arg * arg)
 write_file_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	FILE * fp = xopen_with_backup (arg->text.buf, "w");
-	*end = 0;
-	if (!fp)
-	{
-		io_error_msg ("Can't open file '%s':%s", arg->text.buf, err_msg ());
-		return "";
-	}
-	else
-	{
-		arg->val.fp = fp;
-		return 0;
-	}
+	return 0;
 }
 
 	void
 write_file_destroy (struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	int num;
-
-	num = xclose (arg->val.fp);
-	if (num)
-		io_error_msg ("Can't close '%s': Error code %d: %s",
-				arg->text.buf, num, err_msg ());
 }
 
 /* As a special case, cmd_loop makes sure that keyseq arguments are only read
@@ -260,7 +159,6 @@ write_file_destroy (struct command_arg * arg)
 keyseq_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	*end = 0;
 	return 0;
 }
 
@@ -269,14 +167,7 @@ keyseq_verify (char ** end, struct command_arg * arg)
 keymap_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	char * start = *end;
-	const char * error = symbol_verify (end, arg);
-	int id;
-	if (error)
-		return error;
-	id = map_idn (start, *end - start);
-	static char nsk[] = "No such keymap.";
-	return (id >= 0 ? (char *) 0 : nsk);
+	return 0;
 }
 
 
@@ -284,35 +175,7 @@ keymap_verify (char ** end, struct command_arg * arg)
 number_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	char * e = *end;
-
-	while (*e && isspace (*e))
-		++e;
-	if (isdigit(*e) || (*e == '-'))
-	{
-		arg->val.integer = astol (end);
-		if (arg->arg_desc[1] == '[')
-		{
-			char * prompt = arg->arg_desc + 2;
-			{
-				long low = 0;
-				long high = -1;
-				low = astol (&prompt);
-				while (*prompt && isspace (*prompt))  ++prompt;
-				if (*prompt == ',') ++prompt;
-				high = astol (&prompt);
-				while (*prompt && isspace (*prompt))  ++prompt;
-				if (*prompt == ']') ++prompt;
-				if (   (low > arg->val.integer)
-						|| (high < arg->val.integer))
-					io_error_msg ("Out of range %d (should be in [%d - %d]).",
-							arg->val.integer, low, high); /* no return */
-			}
-		}
-		return 0;
-	}
-	else
-		return "Not a number.";
+	return 0;
 }
 
 
@@ -320,17 +183,7 @@ number_verify (char ** end, struct command_arg * arg)
 double_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	char * e = *end;
-
-	while (*e && isspace (*e))
-		++e;
-	if (isdigit(*e) || ((*e == '-') && isdigit (*(e + 1))))
-	{
-		arg->val.floating = astof (end);
-		return 0;
-	}
-	else
-		return "Not a number.";
+	return 0;
 }
 
 
@@ -338,20 +191,13 @@ double_verify (char ** end, struct command_arg * arg)
 range_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	union command_arg_val * val = &arg->val;
-	*end = arg->text.buf;
-	if (get_abs_rng (end, &val->range))
-		return "Not a range.";
-	else
-		return 0;
+	return 0;
 }
 
 	const char * 
 string_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	arg->val.string = arg->text.buf;
-	*end = 0;
 	return 0;
 }
 
@@ -364,18 +210,7 @@ string_verify (char ** end, struct command_arg * arg)
 yes_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	if (words_imatch (end, "no"))
-	{
-		pop_unfinished_command ();
-		return "Aborted.";
-	}
-	else if (words_imatch (end, "yes"))
-		return 0;
-	else
-	{
-		setn_edit_line ("", 0);
-		return "Please answer yes or no.";
-	}
+	return 0;
 }
 
 	const char *
@@ -392,10 +227,6 @@ incremental_cmd_verify (char ** end, struct command_arg * arg)
 format_verify (char ** end, struct command_arg * arg)
 {
 	ASSERT_UNCALLED();
-	arg->val.integer = str_to_fmt (*end);
-	if (arg->val.integer < 0)
-		return "Unknown format.";
-	*end = 0;
 	return 0;
 }
 
