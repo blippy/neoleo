6e952b77ca2e96d19bb520377362502c6c6550ea
Mark Carter <alt.mcarter@gmail.com>
2020-01-08 14:46:14 +0000

    Remove obsolete code in value.h

--- src/value.h
+++ src/value.h
@@ -1,24 +1,7 @@
 #pragma once
 
-#include <cassert>
-
-#include "global.h"
-#include "numeric.h"
-
 enum ValType { TYP_NUL=0, // taken to mean 'undefined'
 	TYP_FLT=1, TYP_INT=2, TYP_STR=3, TYP_BOL=4, TYP_ERR=5, TYP_RNG=7 };
-
-union vals {
-	num_t c_n;
-	char *c_s;
-	long c_l;
-	int c_i;
-	int c_err;
-	bool c_b;
-	struct rng c_r;
-};
-
-
 
 
 ValType get_value_t_type(const value_t& val);
