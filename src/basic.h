156d30f69a01a73a2f58f5c0605bb747a1f78916
Mark Carter <alt.mcarter@gmail.com>
2020-05-28 19:51:42 +0100

    Cleaned up basic.h

--- src/basic.h
+++ src/basic.h
@@ -1,8 +1,6 @@
 #pragma once
 /*
- * $Id: basic.h,v 1.6 2000/08/10 21:02:49 danny Exp $
- *
- * Copyright © 1993 Free Software Foundation, Inc.
+ * Copyright (c) 1993 Free Software Foundation, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -37,85 +35,11 @@ enum motion_magic
 extern const int colmagic[9];
 extern  const int rowmagic[9];
 
-extern void noop (void);
-extern void insert_row (int repeat);
-extern void insert_col (int repeat);
-extern void delete_row (int repeat);
-extern void delete_col (int repeat);
-extern void open_window (char *text);
-extern void hsplit_window (void);
-extern void vsplit_window (void);
-extern void close_window (char *text);
-extern void delete_window (void);
-extern void delete_other_windows (void);
-extern void nicely_goto_window (int n);
-extern void goto_minibuffer (void);
-extern void goto_window (char *text);
-extern void other_window (void);
-extern int set_window_option (int set_opt, char *text);
-extern void show_window_options (void);
-
-/* mcarter 21-Dec-216: following function is causing confusion. 
- * I don't think it should be here??
- */
-extern void recenter_window (void);
-
-extern void suspend_oleo (void);
-extern void recalculate (int all);
-extern void kill_oleo (void);
-extern void kill_all_cmd (void);
-extern void redraw_screen (void);
-extern void shift_cell_cursor (int dir, int repeat);
-extern void scroll_cell_cursor (int dir, int repeat);
-extern void goto_region (struct rng *r);
-extern void goto_cell (struct rng * rng);
-extern void upper_left (void);
-extern void lower_left (void);
-extern void upper_right (void);
-extern void lower_right (void);
-extern void mark_cell_cmd (int popmk);
-extern void unmark_cmd (void);
-extern void save_mark_to_cell (struct rng * rng);
-extern void save_point_to_cell (struct rng * rng);
-extern void do_mouse_goto (void);
-extern void do_mouse_mark (void);
-extern void do_mouse_mark_and_goto (void);
-extern void do_mouse_cmd (void (*fn) ());
-extern void mouse_mark_cmd (void);
-extern void mouse_goto_cmd (void);
-extern void mouse_mark_and_goto_cmd (void);
-extern void imove (struct rng * rng, int ch);
-extern void inc_direction (int count, int page_rule, int hack_magic);
-extern void beginning_of_row (int count);
-extern void end_of_row (int count);
-extern void beginning_of_col (int count);
-extern void end_of_col (int count);
-extern void set_region_formula (struct rng * rng, char * str);
-extern void goto_edit_cell (int c);
-extern void goto_set_cell (int c);
-extern void run_shell(char *cmd);
-extern void read_file_and_run_hooks (FILE * fp, int ismerge, const char * name);
-extern void toggle_load_hooks (int turn_on);
-extern void write_cmd (FILE *fp, const char * name);
-extern void read_cmd (FILE *fp, char * name);
-extern void read_merge_cmd (FILE *fp);
-extern void write_reg_cmd (FILE *fp, struct rng *rng);
-extern void set_region_height (struct rng * rng, char * height);
-extern void set_region_width (struct rng * rng, char * width);
-extern void set_region_protection (struct rng * rng, int prot);
-extern void set_region_alignment (struct rng * rng, int align);
-extern void set_region_format (struct rng * rng, int fmt);
-extern void set_def_height (char * height);
-extern void set_def_width (char * width);
-extern void set_def_protection (int prot);
-extern void set_def_alignment (int align);
-extern void set_def_format (int fmt);
-extern void set_def_font (void);
-extern void define_usr_fmt (int fmt, char * pos_h, char * neg_h, char * pos_t,
-		char * neg_t, char * zero, char * comma, char * decimal,
-		char * precision, char * scale);
-extern void set_auto_direction (enum motion_magic magic);
-extern void auto_move (void);
-extern void set_cell_alignment_left();
-extern void set_cell_alignment_right();
+void delete_row (int repeat);
+void set_def_format (int fmt);
+void set_cell_alignment_left();
+void set_cell_alignment_right();
 void insert_1row();
+void recalculate (int all);
+void write_cmd (FILE *fp, const char * name);
+void read_file_and_run_hooks (FILE * fp, int ismerge, const char * name);
