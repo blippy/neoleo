d2f5cfdd9e9824e7961ab5f2b009990cfffff421
Mark Carter <alt.mcarter@gmail.com>
2020-05-28 14:26:06 +0100

    Simplified numeric.h
    We no longer consider using decimal

--- src/numeric.h
+++ src/numeric.h
@@ -2,44 +2,9 @@
 
 #include <stdint.h>
 
-#if 0
-	#ifdef __cplusplus
-	#define USE_DECIMAL 1
-	#else
-	#define USE_DECIMAL 0
-	#endif
-
-	#ifndef _GLIBCXX_USE_DECIMAL_FLOAT
-	#undef  USE_DECIMAL
-	#define USE_DECIMAL 0
-	#pragma message "Falling back to doubles instead of decimals"
-	#endif
-#endif
-
-// mcarter 04-Nov-2017 I think we're OK with just
-// using good old-fashioned doubles
-#undef USE_DECIMAL
-
-#if USE_DECIMAL
-
-	#ifdef __cplusplus
-		#include <decimal/decimal>
-		typedef std::decimal::decimal64::__decfloat64 _num_t;
-	#else
-		typedef _Decimal64 _num_t;
-	#endif
-	#define E100 1E100DD
-	#define NUM_HUNDREDTH 0.01DD
-	#define NUM_TEN 10.0DD
-#else
-	typedef double _num_t;
-	#define E100 1E100
-	#define NUM_HUNDREDTH 0.01
-	#define NUM_TEN 10.0
-#endif
+typedef double _num_t;
+#define E100 1E100
+#define NUM_HUNDREDTH 0.01
+#define NUM_TEN 10.0
 
 typedef _num_t num_t;	
-//typedef  uint8_t num_t[8];
-
-//typedef struct { uint8_t bytes[8]; }  num_s;
-//typedef  uint8_t num_s[8];
