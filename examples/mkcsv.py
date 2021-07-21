be2fad934df6ea184922d78eefd587f06ada7320
Mark Carter <alt.mcarter@gmail.com>
2021-07-21 11:55:52 +0100

    new file:   mkcsv.py
    Module seems broken, though.

---
+++ examples/mkcsv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Hmmm. doesn't seem to work
+
+from io import StringIO
+from sylk_parser import SylkParser
+
+parser = SylkParser("loop.oleo")
+
+fbuf = StringIO()
+parser.to_csv(fbuf)
+
+test_results = fbuf.getvalue()
+print test_results
