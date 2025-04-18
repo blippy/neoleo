a3a0b7f2ab44a824b9a6cbb8119e2632f487ba70
Mark Carter <alt.mcarter@gmail.com>
2025-04-18 09:09:24 +0100

    Modules
    Module support is pretty bad.

--- src/mod.cc
+++ src/mod.cc
@@ -1,16 +1,16 @@
 // 2025-04-18 Created for testing purposes
 // 25/4 Intellisense doesn't support modules yet
 
-#include <string_view>
+//#include <string_view>
 
 export module mod;
 
 //#include "global.h"
-
+//#include <string_view>
 //#include <typeinfo>
 //import <string_view>;
 
-using namespace std::literals;
+//using namespace std::literals;
 
 export const char* mod_hi()
 {
@@ -18,8 +18,11 @@ export const char* mod_hi()
 
 }
 
+// 25/4 good luck getting this working, even though it is simple
+/*
 export std::string_view mod_hi_sv()
 {
 	return  "module mod says hi"sv;
 
 }
+*/	
