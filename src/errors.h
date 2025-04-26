2281c03b7d1ad72da42a30606e07ac2a483d4102
Mark Carter <alt.mcarter@gmail.com>
2025-04-26 18:10:22 +0100

    moved err_t from errors.h to value.h
    Err_t is more a property of a value than it is an runtime error.

--- src/errors.h
+++ src/errors.h
@@ -24,27 +24,6 @@
 #include <string>
 
 
-/* These are all the possible error codes that eval_expression() can return. */
-enum { ERR_CMD =1, BAD_INPUT,  NON_NUMBER,  NON_STRING, NON_BOOL, NON_RANGE,  OUT_OF_RANGE, NO_VALUES,
-	DIV_ZERO, BAD_NAME, NOT_AVAL, PARSE_ERR, NO_OPEN, NO_CLOSE, NO_QUOTE, BAD_CHAR, BAD_FUNC, CYCLE};
-
-//#define ERR_MAX		18
-
-typedef struct err_t { int num; std::string what; } err_t;
-//extern char *ename[];
-#define CCC1(x) x
-inline const char *ename[] = 
-{
-	CCC1("#WHAT?"),
-	CCC1("#ERROR"), CCC1("#BAD_INPUT"), CCC1("#NON_NUMBER"), CCC1("#NON_STRING"),
-	CCC1("#NON_BOOL"), CCC1("#NON_RANGE"), CCC1("#OUT_OF_RANGE"), CCC1("#NO_VALUES"),
-	CCC1("#DIV_BY_ZERO"), CCC1("#BAD_NAME"), CCC1("#NOT_AVAIL"), CCC1("#PARSE_ERROR"),
-	CCC1("#NEED_OPEN"), CCC1("#NEED_CLOSE"), CCC1("#NEED_QUOTE"), CCC1("#UNK_CHAR"),
-	CCC1("#UNK_FUNC"), CCC1("#CYCLE"),
-	CCC1(0)
-};
-
-
 
 
 
--- src/value.h
+++ src/value.h
@@ -1,9 +1,31 @@
 #pragma once
 
-#include "errors.h"
+//#include "errors.h"
 
 enum ValType { TYP_NUL=0, // taken to mean 'undefined'
 	TYP_FLT=1, TYP_INT=2, TYP_STR=3, TYP_BOL=4, TYP_ERR=5, TYP_RNG=7 };
+
+/* These are all the possible error codes that eval_expression() can return. */
+enum { ERR_CMD =1, BAD_INPUT,  NON_NUMBER,  NON_STRING, NON_BOOL, NON_RANGE,  OUT_OF_RANGE, NO_VALUES,
+	DIV_ZERO, BAD_NAME, NOT_AVAL, PARSE_ERR, NO_OPEN, NO_CLOSE, NO_QUOTE, BAD_CHAR, BAD_FUNC, CYCLE};
+
+//#define ERR_MAX		18
+
+typedef struct err_t { int num; std::string what; } err_t;
+//extern char *ename[];
+#define CCC1(x) x
+inline const char *ename[] =
+{
+	CCC1("#WHAT?"),
+	CCC1("#ERROR"), CCC1("#BAD_INPUT"), CCC1("#NON_NUMBER"), CCC1("#NON_STRING"),
+	CCC1("#NON_BOOL"), CCC1("#NON_RANGE"), CCC1("#OUT_OF_RANGE"), CCC1("#NO_VALUES"),
+	CCC1("#DIV_BY_ZERO"), CCC1("#BAD_NAME"), CCC1("#NOT_AVAIL"), CCC1("#PARSE_ERROR"),
+	CCC1("#NEED_OPEN"), CCC1("#NEED_CLOSE"), CCC1("#NEED_QUOTE"), CCC1("#UNK_CHAR"),
+	CCC1("#UNK_FUNC"), CCC1("#CYCLE"),
+	CCC1(0)
+};
+
+
 
 // monostate basically means "empty" as a valid type
 typedef std::variant<std::monostate, num_t, std::string, err_t, rng_t, bool_t> value_t;
