f8addd568f71419f8d16eb279324a70d1d3791ef
Mark Carter <alt.mcarter@gmail.com>
2025-05-02 08:32:36 +0100

    Added config.pl
    I'm going to build stuff using a perl script

---
+++ config.pl
@@ -0,0 +1,23 @@
+#/usr/bin/env perl
+
+## cc
+basic.cc errors.cxx logging.cxx
+cell.cc value.cxx
+io-2019.cc errors.cxx logging.cxx win.cxx
+io-headless.cc logging.cxx mod.cxx
+io-utils.cc errors.cxx logging.cxx value.cxx
+main.cc logging.cxx
+menu-2025.cc logging.cxx
+
+oleofile.cc
+parser-2019.cc
+regions.cc
+cheet.cc
+spans.cc
+tests.cc
+utils.cc
+
+# cxx
+value.cxx errors.cxx
+
+
