68b48f9dd7339eee03238cad7704b32139c8bf03
Mark Carter <alt.mcarter@gmail.com>
2025-08-20 16:04:58 +0100

    Better testing
    Not all tests work, but a better testing mechanism
    is starting to form.

--- tests/basic.sh
+++ tests/basic.sh
@@ -4,4 +4,9 @@
 
 #echo "This just see if 'cmake test' works"
 #echo "Called with arg1: $1"
+#env
+echo "FOO is $FOO"
+echo "abs_top_builddir is $abs_top_builddir"
+echo "SRCDIR is $SRCDIR"
+echo "BUILDDIR is $BUILDDIR"
 exit 0
