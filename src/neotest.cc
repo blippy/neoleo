5a6fdb223c66214a274b8a2902f75f387219a844
Mark Carter <alt.mcarter@gmail.com>
2025-10-15 16:32:42 +0100

    Added a standalone test: neotest
    It works.
    
    Perhaps I should loading and saving a file to get more ambitious.

--- src/neotest.cc
+++ src/neotest.cc
@@ -4,13 +4,23 @@
  * Doing a general test of using the spreadsheet from a separate program
  *  Created on: 15 Oct 2025
  *      Author: pi
+ * Works, too. That was surprisingly easy
  */
 
+//#include <exception>
+#include <iostream>
 #include <stdio.h>
+#include <string>
+
+#include "parser-2019.h"
+
+using namespace std;
 
 int main()
 {
 	puts("neotest says hi");
+	auto result = set_and_eval(1, 1, "7", false);
+	cout << "result is " << result << ", should be 7" << endl;
 
 	return 0;
 }
