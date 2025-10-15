774241f2e313d1b733eeda3609b3d6ab1518c297
Mark Carter <alt.mcarter@gmail.com>
2025-10-15 16:05:42 +0100

    Added neotest.cc

---
+++ src/neotest.cc
@@ -0,0 +1,18 @@
+/*
+ * neotest.cc
+ *
+ * Doing a general test of using the spreadsheet from a separate program
+ *  Created on: 15 Oct 2025
+ *      Author: pi
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	puts("neotest says hi");
+
+	return 0;
+}
+
+
