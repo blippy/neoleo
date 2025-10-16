4fa9a4fcf0dc9135a77b34c2cf0e73c0628d01df
Mark Carter <alt.mcarter@gmail.com>
2025-10-16 20:08:24 +0100

    Added pids.py
    An experiment in getting neoleo to work as a ps -e
    viewer.

---
+++ examples/pids.py
@@ -0,0 +1,33 @@
+import psutil
+procs = []
+for p in psutil.process_iter():
+	procs.append([p.pid, p.name()])
+
+#print(procs)
+
+def keyer(x): return x[1].lower()
+#print(procs)
+procs.sort(key = keyer)
+#procs = procs1
+#print(procs)
+
+print("\n".join(["g", "1", "1", "I"])) # got to R1C1 and insert rowwise
+for p in procs:
+	print(p[0])
+	print('"' + p[1] + '"')
+	print(";")
+print(".")
+
+
+
+#print("\n".join(["g", "1", "2", "i"])) # got to R1C1 and insert columnwise
+#print(";")
+#for p in procs:
+#	print('"', p[1], '"', sep = '')
+#print(".")
+
+
+print("\n".join(["g", "1", "1"]))
+print("visual")
+
+
