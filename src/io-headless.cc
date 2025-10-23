3d67b54d02944fbab2b496e9957b2cf6626fb501
Mark Carter <alt.mcarter@gmail.com>
2025-10-23 17:16:49 +0100

    Clean up headless processing

--- src/io-headless.cc
+++ src/io-headless.cc
@@ -47,30 +47,6 @@ static string get_word(std::istream& is){
 	return result;
 }
 
-#if 0
-// FN getline_from_fildes .
-static std::string getline_from_fildes (int fildes, bool& eof)
-{
-	char ch;
-	std::string line;
-	while(true) {
-		eof = read(fildes, &ch, 1) == 0;
-		if(eof) return line;
-
-		if(ch == '\n') {
-			//ofs << "line: " << line << endl;
-			//exec_cmd(line, fildes);
-			//line = "";
-			return line;
-		} else {
-			line += ch;
-		}
-	}
-
-	return line;
-}
-// FN-END
-#endif
 
 static void hl_eat_ws(stringstream& ss)
 {
@@ -123,13 +99,6 @@ void set_cell_input_1 (CELLREF r, CELLREF c, const string& formula)
 }
 
 
-#if 0
-static string hl_getline (int fildes)
-{
-	bool eof;
-	return getline_from_fildes(fildes, eof);
-}
-#endif
 
 string to_hex(long n)
 {
@@ -159,9 +128,7 @@ static void hless_dump_sheet()
 
 bool reading(std::istream& is, string& line)
 {
-	//bool eof;
 again:
-	//line = getline_from_fildes(fildes, eof);
 	getline(is, line);
 	if(line == ".") return false;
 	if(line.size() == 0 && is.eof()) return false;
@@ -171,10 +138,9 @@ again:
 
 static void insert_columnwise(std::istream& is)
 {
-	log_func();
+	//log_func();
 	string line;
 	while(reading(is, line)) {
-		log("line is ", line);
 		if(line == ";") { cucol++; 	curow=1; continue;}
 		if(line == "")  { curow++; continue;}
 		set_cell_input_1(curow, cucol, line);
@@ -203,9 +169,6 @@ static void type_cell()
 void hl_write_file()
 {
 	string name = FileGetCurrentFileName();
-	
-	//log("writing file:", name);
-	
 	FILE *fp = fopen(name.c_str(), "w");
 	if(!fp) {
 		_sys_ret = 1; 
@@ -213,7 +176,6 @@ void hl_write_file()
 		return;
 	}
 
-	//assert(fp);
 	write_cmd(fp, name.c_str());
 	fclose(fp);
 
@@ -223,7 +185,6 @@ static void hl_write_file(std::istream& is)
 {
 	string filename;
 	getline(is, filename);
-	//log("_write_file:called:", filename);
 	if(filename.size() > 0) FileSetCurrentFileName(filename);
 	hl_write_file();
 
@@ -236,22 +197,10 @@ static void hl_write_file(std::istream& is)
 
 static void hl_goto_cell(std::istream& is)
 {
-	//std::size_t pos{};
-	//string line;
-	log_func();
+	//log_func();
 	try {
 		CELLREF r = stol(get_word(is));
 		CELLREF c = stol(get_word(is));
-		//CELLREF r, c;
-		//is >> r;
-		log("r=", r);
-		//is >> c;
-		log("c=", c);
-
-		//getline(is, line);
-		//auto r = (CELLREF) stol(line);
-		//getline(is, line);
-		//auto c = (CELLREF) stol(line);
 		curow = r;
 		cucol = c;
 	} catch(...) {
@@ -279,12 +228,10 @@ static void _type_sheet()
 
 static void hl_exit(std::istream& is)
 {	
-	//cout << "exit called\n";
 	string str;
 	getline(is, str);
 
 	if(str == "$?") {
-		//log("calling hl_exit with status ", _sys_ret);
 		exit(_sys_ret ? 1 : 0); // return numbers can be too high for our purposes
 	}
 
@@ -297,7 +244,6 @@ static void hl_exec(std::istream& is)
 {
 	string command;
 	getline(is, command);
-	//command = is.rdbuf();
 	_sys_ret = system(command.c_str());
 }
 
@@ -305,14 +251,8 @@ static void hl_exec(std::istream& is)
 // 25/05 Started. Very rough at this stage!
 static void hl_print_row (std::istream& is)
 {
-	// assume for now that we only want to print the first row
-	// and that there are 80 columns
-	//std::array<int, 80> row{-1};
-
-	//log("hl_print_row:enter:", arg);
 	stringstream ss;
 	ss << is.rdbuf();
-	//auto ss = is;
 	int row_start = 1;
 	int row_end = 1;
 	hl_scan_range(ss, row_start, row_end);
@@ -323,11 +263,6 @@ static void hl_print_row (std::istream& is)
 	if(hl_scan_char(ss, 'c'))
 		hl_scan_range(ss, col_start, col_end);
 
-
-
-	//int row = row_start;
-	//log("hl_print_row: row:", row);
-	//int row = std::max(1, atoi(arg.c_str()));
 	for (int row = row_start; row <= row_end; row++) {
 		for (int col = col_start; col <= col_end; col++) {
 			int w = get_width(col);
@@ -344,7 +279,6 @@ static void hl_print_row (std::istream& is)
 		}
 		cout << endl;
 	}
-	//log("hl_print_row:exiting");
 
 }
 // FN-END
@@ -368,7 +302,6 @@ static void process_headless_line(const std::string& str, istream& rest_of_strea
 	std::istringstream is(str);
 	string cmd{get_word(is)};
 
-	//auto is = [&cmd](string s) { return cmd == s; };
 	if( cmd == "!") { hl_exec(is); }
 	else if(cmd == "cal") {column_align_left();}
 	else if(cmd == "clr-sheet") {clear_spreadsheet();}
@@ -399,61 +332,23 @@ static void process_headless_line(const std::string& str, istream& rest_of_strea
 	cout << std::flush;
 }
 
-#if 1
 static void _repl(std::istream& is)
 {
 	string line;
 	while( !Global_definitely_quit && std::getline(is, line)) {
 		process_headless_line(line, is);
 	}
-
-
 }
 
-#else
-static void _repl(int fildes)
-{
-	//log("_repl:start");
-	while(!Global_definitely_quit) {
-		try {
-			bool eof;
-			string line = getline_from_fildes(fildes, eof);
-			process_headless_line(line, fildes);
-			if(Global_definitely_quit) close(fildes);
-			if(eof) { Global_definitely_quit = true; }
-		} catch (OleoJmp& e) {
-			cerr << e.what() << endl;
-		}
-	}
-}
-#endif
 
 
 void headless_main() // FN
 {
-#if 0
-	// 25/10 test of parser - seems to work
-	ranges_t predecs;
-	string s{"life()"};
-	Expr expr{parse_string(s, predecs, curow, cucol)};
-	Tour tour;
-	value_t val{eval_expr(tour, expr)};
-	//cout << fmt_value(val) << endl;
-#endif
-
-#if 1
 	_repl(std::cin);
-#else
-
-	//cout << mod_hi() << endl;
-	constexpr int fildes = STDIN_FILENO;
-	_repl(fildes);
-#endif
 }
 
 int headless_script(const char* script_file)
 {
-#if 1
 	std::ifstream in(script_file);
 	if(!in.is_open()) {
 		cerr << "? Couldn't script file:" << script_file << endl;
@@ -462,19 +357,6 @@ int headless_script(const char* script_file)
 
 	_repl(in);
 
-#else
-	//log("headless_script:started");
-	int fildes = open(script_file, O_RDONLY);
-	if(fildes == -1) {
-		cerr << "? Couldn't script file:" << script_file << endl;
-		return 1;
-	}
-
-	//log("headless_script:calling repl");
-	_repl(fildes);
-	//log("headless_script:finished repl");
-	close(fildes);
-#endif
 	return 0;
 }
 
