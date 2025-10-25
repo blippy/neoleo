33cde460d80c08238081b760566b97960736e83a
Mark Carter <alt.mcarter@gmail.com>
2025-10-25 19:07:24 +0100

    Reinstated parser test

--- tests/parser.sh
+++ tests/parser.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 # test parser created in 2019
 
-#TMPFILE=`mktemp`
-#NEO=$ABUILDDIR/../src/neoleo
-#$NEO -T parser2019 > $TMPFILE
-#set_productions parser.rep
-#mv $TMPFILE $OUTFILE
-#diff $OUTFILE $VERFILE
-
-$BUILDDIR/src/neoleo -T parser2019
+NEO=`realpath ../src/neoleo`
+$NEO -T parser2019
