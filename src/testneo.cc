b161a4ec51e329e0aa6ac464ce31db518fd70a7c
Mark Carter <alt.mcarter@gmail.com>
2025-10-31 11:21:27 +0000

    More test consolidation

--- src/testneo.cc
+++ src/testneo.cc
@@ -21,6 +21,152 @@ using namespace std;
 
 
 
+static bool all_pass = true; // all the checks have passed so far
+
+bool run_parser_2019_tests ();
+int run_clear_test();
+
+//import value;
+
+void check(std::string got, std::string expected, std::string msg)
+{
+	//cout << msg << "\t";
+	bool ok = got == expected;
+	if(ok) {
+		cout << "PASS";
+	} else {
+		cout << "FAIL";
+		all_pass = false;
+	}
+	cout << " " << msg << " Expected "  << expected << ", got " << got << "\n";
+
+}
+
+
+void check(bool ok, std::string msg)
+{
+	if(!ok) all_pass = false;
+	std::string s = ok? "PASS" : "FAIL";
+	std::cout << s << " " << msg << std::endl;
+
+}
+
+
+
+
+
+template<typename... Args>
+std::string format_sub_test(Args... args)
+{
+	return string_format(args...);
+}
+
+bool format_tests()
+{
+	auto check_fmt = [](num_t v, const std::string& s) {
+		bool ok = string(pr_flt(v, &fxt, 2)) == s;
+		check(ok, "check_fmt: " + s);
+	};
+	check_fmt(24.6f, "24.60");
+	check_fmt(16.36f, "16.36"); // a source of potential rounding oddities
+	check_fmt(0.0f,  "0.00");   // issue #10
+	check_fmt(1.0f,  "1.00");
+
+
+	check(pad_left("hello", 7) == "  hello", "padleft");
+	check(pad_right("hello", 7) == "hello  ", "padright");
+
+	string s1{"Hello %s, meaning of life is %d"};
+	string s2 = format_sub_test(s1, "world", 42);
+	//cout << s2 << "\n";
+	check(s2,  "Hello world, meaning of life is 42", "format_sub_test");
+
+	time_t t =1745492070;
+	check(fmt_std_date(t), "2025-04-24", "fmt_std_date");
+	return all_pass;
+}
+
+
+// a test that always fails
+bool fail()
+{
+	return false;
+}
+
+// a test that always passes
+bool pass()
+{
+	return true;
+}
+
+
+
+
+//#define REQUIRE(x) if(!(x)) cout << __FILE__ << ":" << __LINE__  << ":"<<  #x << " FAIL\n";
+bool test_values()
+{
+	//cout << "test_values ... ";
+	value_t v1 =1.0, v2 = 1.0;
+	check(v1==v2, "v1==v2");
+	v2 = 3.0;
+	check(v1!=v2, "v1!=v2");
+	return all_pass;
+}
+
+
+
+
+
+
+bool headless_tests()
+{
+	//bool all_pass = true;
+	cout << "Running tests: " << option_tests_argument << "\n";
+
+	using func_t = struct { string name; std::function<bool()> func; };
+	vector<func_t> funcs = {
+		{"clear",	run_clear_test},
+		//{"decomp-01", test_cecomp_01};
+		{"fail",	fail},
+		{"fmt",		format_tests},
+		{"parser2019",	run_parser_2019_tests},
+		{"pass",	pass},
+		//{"regular", 	run_regular_tests},
+		{"vals", 	test_values}
+	};
+
+
+	// maybe test is referred by a number
+	auto idx = to_int(option_tests_argument).value_or(-1);
+	int fsize = static_cast<int>(funcs.size());
+	if(0 <= idx && idx < fsize ) {
+		auto fn = funcs[idx].func;
+		return fn();
+	}
+
+	// Wasn't referred by number, so possibly a name
+	for(const auto &f : funcs) {
+		if(f.name == option_tests_argument) {
+			auto fn = f.func;
+			return fn();
+		}
+	}
+
+	using std::cerr;
+	// use input was neither a number or valid description
+	if(option_tests_argument != "") cerr << "Test not found: `" << option_tests_argument << "'\n";
+	cerr << "Available tests are:\n";
+	for(auto idx1 =0; idx1 < fsize; idx1++) {
+		cerr << "\t" << idx1 << " " << funcs[idx1].name << "\n";
+	}
+
+
+	return false;
+}
+
+
+
+
 bool check_result(CELLREF r, CELLREF c, const string& expecting)
 {
 	string res = cell_value_string(r, c, 0);
