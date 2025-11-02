6829715a40ea57bd819f378be2c7f18922352e07
Mark Carter <alt.mcarter@gmail.com>
2025-11-02 18:01:50 +0000

    modified:   oleo.tcl

--- tcl/oleo.tcl
+++ tcl/oleo.tcl
@@ -1,8 +1,21 @@
 package provide oleo 0.1
 
 namespace eval ::oleo {
-	namespace export oleo-hi
+	namespace export incra oleo-hi
 }
+
+# increment array
+# For example, see 25.tcl
+proc ::oleo::incra {arr key by} {
+        upvar 1 $arr a
+        if {[info exists a($key)]} {
+                set a($key) [expr $a($key) + $by]
+        } else {
+                set a($key) $by
+        }
+}
+
+
 
 proc ::oleo::oleo-hi {} {
 	puts "oleo module says 'hi'"
