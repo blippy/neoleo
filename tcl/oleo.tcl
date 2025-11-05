9490388aa3e2753cd4ecc20d5cfec087cac57513
Mark Carter <alt.mcarter@gmail.com>
2025-11-05 11:59:15 +0000

    Enhanced oleo.tcl. Improved docs

--- tcl/oleo.tcl
+++ tcl/oleo.tcl
@@ -15,9 +15,32 @@ proc ::oleo::incra {arr key by} {
         }
 }
 
+# nicer implementation of the for loop
+# e.g. 
+# forn i 1 10 1 { 
+#	# for i from 1 10 step 1
+#   puts "i= $i"
+# }:w
+
+proc ::oleo::forn {varName from to step block} {
+        upvar 1 $varName var
+        for {set var $from} { ($var<=$to && $step>0) || ($var>=$to && $step<=0)} {set var [expr $var + $step]} {
+                uplevel 1 $block
+        }
+}
 
 
 proc ::oleo::oleo-hi {} {
 	puts "oleo module says 'hi'"
 }
+
+
+# set the cells in a column as a list
+proc ::oleo::get-col {n} {
+        ::oleo::forn r 1 [max-row] 1 {
+                set v [get-cell $r $n]
+                lappend result $v
+        }       
+        return $result
+}       
 
