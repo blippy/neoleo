c256f09beacc1784c474471c1772d68a7fad6a56
Mark Carter <alt.mcarter@gmail.com>
2025-11-06 20:24:14 +0000

    Changes to the build system
    Throwing stuff at the wall.

---
+++ src/swig.cmake
@@ -0,0 +1,41 @@
+# 25/11 added
+# puzzling out the whole swig mess
+
+find_package(SWIG REQUIRED)
+
+
+# generate tickle_wrap..c into the build/src directory
+set(wrapper_cc ${CMAKE_CURRENT_BINARY_DIR}/tickle_wrap.cc)
+add_custom_target(swigly
+	#PRE_BUILD
+	COMMAND swig -c++ -tcl  -o ${wrapper_cc} ${CMAKE_CURRENT_SOURCE_DIR}/tickle.i
+	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/tickle.i
+	BYPRODUCTS ${wrapper_cc}
+	VERBATIM
+	USES_TERMINAL
+	COMMENT "Generating swig wrapper source file"
+) 
+
+#add_library 
+
+
+
+#find_package(SWIG REQUIRED)
+#include(UseSWIG)
+#include_directories(BEFORE ${CMAKE_CURRENT_SOURCE_DIR}) # for all targets
+#set_property(SOURCE tickle.i PROPERTY CPLUSPLUS ON)
+#set(PLOP libploppy)
+#add_library(ploppy_do SHARED ${common_srcs})
+#swig_add_library(ploppy
+#	TYPE SHARED
+#	#TYPE STATIC
+#	LANGUAGE  tcl
+	#OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/Generated"
+	#OUTFILE_DIR "${CMAKE_CURRENT_BINARY_DIR}/Wrapper"
+#	SOURCES tickle.i 
+#)
+#swig_link_libraries(ploppy ploppy_do)
+#add_library(libploppy STATIC ploppy.so)
+#install(TARGETS ${PLOP} DESTINATION lib)
+#install(CODE "message(\"Remember to run sudo ldconfig to update libraries\")")
+
