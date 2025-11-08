d63fdf6db7037c15ae3b1bd32ca52f89ce492f06
Mark Carter <alt.mcarter@gmail.com>
2025-11-08 11:15:41 +0000

    logged path
    Useful for starting up on desktop

--- src/main.cc
+++ src/main.cc
@@ -168,6 +168,7 @@ void run_nonexperimental_mode(int argc, char** argv) //, int command_line_file, 
 	extern void tickle_main();
 	using namespace std::literals;
 	set_def_format(155); // which is "general.float", believe it or not
+	log("\n=== Started as ", argv[0]);
 
 	if (argc - optind == 1) {
 		if (FILE *fp = fopen (argv[optind], "r")) {
