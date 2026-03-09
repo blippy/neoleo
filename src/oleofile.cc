4f5f46571bc338ef73d437cb75475ce07a1c5049
Mark Carter <alt.mcarter@gmail.com>
2026-03-09 10:13:33 +0000

    Removed read_mp_usr_fmt

--- src/oleofile.cc
+++ src/oleofile.cc
@@ -137,88 +137,6 @@ static long astol (char **ptr)
 }
 
 
-void read_mp_usr_fmt (char *ptr) // FN
-{
-	int usr_n = -1;
-	int n_chrs = 0;
-	char *p = ptr;
-
-	while (*p == ';')
-	{
-		*p++ = '\0';
-		switch (*p++)
-		{
-			case 'N':
-				usr_n = astol (&p) - 1;
-				break;
-			case 'H':
-				switch (*p++)
-				{
-					case 'P':
-						//i = 0;
-						break;
-					case 'N':
-						//i = 1;
-						break;
-					default:
-						goto badline;
-				}
-				goto count_chars;
-			case 'T':
-				switch (*p++)
-				{
-					case 'P':
-						//i = 2;
-						break;
-					case 'N':
-						//i = 3;
-						break;
-					default:
-						goto badline;
-				}
-				goto count_chars;
-
-			case 'Z':
-				//i = 4;
-				goto count_chars;
-
-			case 'C':
-				//i = 5;
-				goto count_chars;
-
-			case 'D':
-				//i = 6;
-				goto count_chars;
-
-			case 'P':
-				//i = 7;
-				goto count_chars;
-
-			case 'S':
-				//i = 8;
-				goto count_chars;
-
-count_chars:
-				n_chrs++;
-				while (*p && *p != ';')
-				{
-					p++;
-					n_chrs++;
-				}
-				break;
-
-			default:
-badline:
-				raise_error("Unknown OLEO line %s", ptr);
-				return;
-		}
-	}
-	if (*p || usr_n < 0 || usr_n > 15)
-		goto badline;
-
-	
-}
-
 
 
 
@@ -433,12 +351,6 @@ void oleo_read_file (FILE *fp)
 			case 'W':
 				oleo_read_window_config (ptr + 2);
 				break;
-			case 'U':
-				/* JF extension:  read user-defined formats */
-				read_mp_usr_fmt (ptr + 1);
-				break;
-				/* JF extension: read uset-settable options */
-
 			case '#':		/* comment line -- ignored */
 			case '%':		/* Font or pixel size data. */
 			case 'B':		/* Boundry field, ignored */
